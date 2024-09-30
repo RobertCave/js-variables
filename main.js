41c5951d78c25d4a249779c701a49e1007e78dc7
Roberto Lacava <roberto.lacava@gmail.com>
2024-09-30 12:56:36 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+// Selfwork Variabili
+// Crea un file HTML e inserisci un tag script prima della chiusura del body. Poi al suo interno esegui i seguenti passaggi:
+
+//     dichiara una variabile num1, e assegnale un numero qualsiasi
+//     dichiara una variabile num2, e assegnale un numero qualsiasi
+//     visualizza a schermo “Il valore della variabile num1 e' tot“. Fai la stessa cosa per num2
+//     dichiara una variabile chiamata stringa, e assegnale come valore una stringa qualsiasi
+//     stampa a schermo “Il valore della variabile stringa e' tot“
+//     sostituisci il valore della variabile stringa con un qualsiasi altro valore e poi stampalo a schermo.
+//     dichiara una costante chiamata PIGRECO, e assegnale un valore di 3.14159265359
+//     visualizza a schermo “il valore di PIGRECO e' tot”
+//     prova a cambiare il valore della costante e devi cosa succede
+
+
+
+let num1 = 37;
+let num2 = 53;
+
+console.log("Il valore della variabile num1 è di: " + num1 );
+console.log("Il valore della variabile num2 è di: " + num2 );
+
+let stringa = "Javascript";
+
+console.log("Il valore della variabile stringa è: " + stringa);
+
+stringa = "Js";
+
+console.log("Il valore della variabile stringa adesso è: " + stringa);
+
+const PIGRECO = 3.14159265359;
+
+console.log("Il valore della costante PIGRECO è: " + PIGRECO);
+
+PIGRECO = 99; // Errore nell'assegnare un nuovo valore ad una costante
